f047849dc08673cdb3d7a7ed373e94efefdb931a
WeezStorm <49243135+WeezStorm@users.noreply.github.com>
2019-05-21 21:40:29 +0200

    jjj
    zzz

--- Banblock By WeeZz.php
+++ Banblock By WeeZz.php
@@ -1,23 +1,8 @@
 <?php
 
-/*
- * BlockCMD - A PocketMine-MP plugin to block certain commands from being used by players
- * Copyright (C) 2017 Kevin Andrews <https://github.com/kenygamer/BlockCMD>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-*/
-
 declare(strict_types=1);
 
-namespace BlockCMD;
+namespace WeeZz;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
